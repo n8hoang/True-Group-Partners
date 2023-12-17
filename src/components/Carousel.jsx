1640cee6a506d742bc826390af1ea55d8573652a
Nathan Hoang <sliqq123@gmail.com>
2023-12-17 12:38:26 -0800

    converted images to webp and added md screen styling

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,7 +1,12 @@
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Parallax, Pagination, Navigation , Autoplay} from 'swiper/modules';
-import natureImg from '../assets/nature.jpg'
+import natureImg from '../assets/nature.webp'
+import workImg from '../assets/work.webp'
+import financeImg from '../assets/finance.webp'
+import officeImg from '../assets/office.webp'
+import stocksImg from '../assets/stocks.webp'
+
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
@@ -28,42 +33,42 @@ export default Carousel = () => {
         }}
         navigation={true}
         modules={[Parallax, Pagination, Navigation, Autoplay]}
-        className="mySwiper h-[250px]"
+        className="mySwiper h-[250px] md:h-[300px]"
     >
         <SwiperSlide>
-            <div className="title mt-5 ml-20 z-10 relative text-3xl w-[250px] h-[100px] flex text-center bg-white/50 text-black" data-swiper-parallax="-300">
+            <div className="title mt-5 ml-20 z-10 relative text-3xl w-[250px] h-[100px] flex text-center bg-white/50 text-black md:mt-10" data-swiper-parallax="-300">
                <p className=' pt-3'>Finance & Accouting</p> 
             </div>
 
             <img src={natureImg} className='absolute z-0 left-0 -top-20 h-auto w-auto bg-cover bg-center'/>
         </SwiperSlide>
         <SwiperSlide>
-            <div className="title z-10 relative  mt-28 ml-auto mr-16 text-3xl w-[250px] h-[100px] flex items-center justify-center bg-white/50 text-black" data-swiper-parallax="-300">
+            <div className="title z-10 relative  mt-28 ml-auto mr-16 text-3xl w-[250px] h-[100px] flex items-center justify-center bg-white/50 text-black md:mt-36" data-swiper-parallax="-300">
                <p>HR & Payroll</p> 
             </div>
 
-            <img src={natureImg} className='absolute z-0 left-0 -top-20 h-auto w-auto bg-cover bg-center'/>
+            <img src={workImg} className='absolute z-0 left-0 -top-20 h-auto w-auto bg-cover bg-center'/>
         </SwiperSlide>
         <SwiperSlide>
             <div className="title mt-5 ml-20 z-10 relative text-3xl w-[250px] h-[100px] flex items-center justify-center bg-white/50 text-black" data-swiper-parallax="-300">
                <p>Taxes</p> 
             </div>
 
-            <img src={natureImg} className='absolute z-0 left-0 -top-20 h-auto w-auto bg-cover bg-center'/>
+            <img src={financeImg} className='absolute z-0 left-0 top-0 h-auto w-auto bg-cover bg-center'/>
         </SwiperSlide>
         <SwiperSlide>
-            <div className="title z-10 relative  mt-28 ml-auto mr-16 text-3xl w-[250px] h-[100px] flex items-center justify-center bg-white/50 text-black" data-swiper-parallax="-300">
+            <div className="title z-10 relative  mt-28 ml-auto mr-16 text-3xl w-[250px] h-[100px] flex items-center justify-center bg-white/50 text-black md:mt-36" data-swiper-parallax="-300">
                <p>Insurance</p> 
             </div>
 
-            <img src={natureImg} className='absolute z-0 left-0 -top-20 h-auto w-auto bg-cover bg-center'/>
+            <img src={stocksImg} className='absolute z-0 left-0 -top-20 h-auto w-auto bg-cover bg-center'/>
         </SwiperSlide>
         <SwiperSlide>
             <div className="title mt-5 ml-20 z-10 relative text-3xl w-[250px] h-[100px] flex items-center justify-center bg-white/50 text-black" data-swiper-parallax="-300">
                <p >Call Center</p> 
             </div>
 
-            <img src={natureImg} className='absolute z-0 left-0 -top-20 h-auto w-auto bg-cover bg-center'/>
+            <img src={officeImg} className='absolute z-0 left-0 -top-20 h-auto w-auto bg-cover bg-center'/>
         </SwiperSlide>
     </Swiper>
   </div>
