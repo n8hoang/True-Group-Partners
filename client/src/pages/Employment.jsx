bf803b18317ffe551b8dad5ae8f25343784be91f
Nathan Hoang <sliqq123@gmail.com>
2023-12-20 20:36:19 -0800

    changed height of employment component

--- client/src/pages/Employment.jsx
+++ client/src/pages/Employment.jsx
@@ -3,7 +3,7 @@ import EmploymentForm from '../components/EmploymentForm'
 
 function Employment() {
   return (
-    <section className='flex flex-col items-center justify-center p-8 bg-slate-500 text-white h-screen'>
+    <section className='flex flex-col items-center justify-center p-8 bg-slate-500 text-white h-[900px]'>
       <h1 className="text-3xl font-semibold text-white mb-8 text-center"> We are constantly looking for the right individuals to join the team of staff accountants, senior accountants, controllers and CFOs.</h1>
       <EmploymentForm/>
     </section>
