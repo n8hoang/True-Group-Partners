9436dc0766e3e50a9be9e5829110176c043b186e
Nathan Hoang <sliqq123@gmail.com>
2023-12-20 20:19:52 -0800

    added path to server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const multer = require('multer');
+const path = require('path');
 const upload = multer({ dest: 'uploads/' });
 require('dotenv').config();
 
